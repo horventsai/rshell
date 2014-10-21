449bddd3e2dba7c25634dbdb554496b986de654b
Horven <htsai010@ucr.edu>
2014-10-21 00:14:22 -0700

    working on other commands and user string parsing

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,33 @@ int main()
 {
 	while(1)
 	{
-		string command;
-		cout << "$ ";
+		//variables
+		char command[] = "test this is a string test to work with";
+		char *pch;
+		vector<string> holding;
+		char *name;
+		size_t len;
+
+		cout << getlogin() << "@" << gethostname(name, len) <<  " $ ";
 		getline(cin, command);
+		
+		pch = strtok(command, " \t\n\r");
+
+		while(pch != NULL)
+		{
+			holding.push_back(pch);
+			pch = strtok(NULL, " \t\n\r");
+		}
+		
+		for(int i = 0; i < holding.size(); i++)
+		{
+			cout << holding.at(i) << " ";
+		}
+		
+		if(command == "exit")
+		{
+			exit(0);
+		}
 	}
 	return 0;
 }
