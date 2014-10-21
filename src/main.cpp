ff15913ab239eb5221b25d760cd3436e239b8c24
Horven <htsai010@ucr.edu>
2014-10-20 19:55:18 -0700

    main can now print prompt with possible user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector>
 #include <string>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+using namespace std;
 
 int main()
 {
-
+	while(1)
+	{
+		string command;
+		cout << "$ ";
+		getline(cin, command);
+	}
 	return 0;
 }
-
